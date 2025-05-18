5b46ad9b47ca3242eeac343b0d4787de2f662d49
David Santos <44490090+dsecuma@users.noreply.github.com>
2025-05-18 22:28:31 +0200

    update

--- oled_controller/buttons_controller.py
+++ oled_controller/buttons_controller.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
-import logging
-import os
 from gpiozero import Button
+from signal import pause
+import os
+import logging
+import subprocess
 
 logging.basicConfig(
     filename='/tmp/buttons.log',
@@ -10,42 +12,56 @@ logging.basicConfig(
     format='%(asctime)s %(levelname)s: %(message)s'
 )
 
-OBS_CLI = "YOUR_OBS-CLI_PATH"
+OBS_CLI = "/home/osc/tmp/venv/bin/obs-cli"
 OBS_PASSW = os.environ.get("OBS_API_PASSWORD")
 OBS_PORT= "4455"
 OBS_CMD = [OBS_CLI, "-p", OBS_PASSW, "-P", OBS_PORT, "record"]
 
-def accion_boton1():
+def update_status_oled(estado):
     try:
-        resultado = subprocess.run(OBS_CMD.append("start"), capture_output=True, text=True, check=True)
-        logging.info(f"Grabación iniciada {resultado.stdout}")
+        with open("/tmp/status-oled", "w") as f:
+            f.write(estado)
+        logging.info(f"OLED status upated: {estado}")
     except Exception as e:
-        logging.error(f"Error al iniciar grabación: {e}")
+        logging.error(f"Error writing /tmp/status-oled: {e}")
 
-def accion_boton2():
+def action_button1():
+    logging.info("Button 1")
     try:
-        resultado = subprocess.run(OBS_CMD.append("stop"), capture_output=True, text=True, check=True)
-        logging.info("Grabación detenida")
+        resultado = subprocess.run(OBS_CMD + ["start"], capture_output=True, text=True, check=True)
+        logging.info(f"Recording...")
+        update_status_oled("RUNNING")
     except Exception as e:
-        logging.error(f"Error al detener grabación: {e}")
+        update_status_oled("ERROR")
+        logging.error(f"Error recording: {e}")
 
-def accion_boton3():
+def action_button2():
+    logging.info("Button 2")
+    try:
+        resultado = subprocess.run(OBS_CMD + ["stop"], capture_output=True, text=True, check=True)
+        logging.info(f"Stopped")
+        update_status_oled("STOPPED")
+    except Exception as e:
+        update_status_oled("ERROR")
+        logging.error(f"Error recording: {e}")
+
+def action_button3():
+    logging.info("Button 3")
     os.system("uptime > /tmp/button3")
-    logging.info("Botón 3 pulsado")
 
-def accion_boton4():
-    # require nopasswd for daemon user
+def action_button4():
+    logging.info("Button 4")
+    # require nopasswd for /sbin/reboot
     os.system("sudo /sbin/reboot")
-    logging.info("Botón 4 pulsado")
 
 buttons = {
-    Button(26): accion_boton1,
-    Button(16): accion_boton2,
-    Button(20): accion_boton3,
-    Button(19): accion_boton4,
+    Button(26): action_button1,
+    Button(16): action_button2,
+    Button(20): action_button3,
+    Button(19): action_button4
 }
 
-for boton, accion in buttons.items():
-    boton.when_pressed = accion
+for button, action in buttons.items():
+        button.when_pressed = action
 
 pause()
--- oled_controller/oled_controller.py
+++ oled_controller/oled_controller.py
@@ -12,17 +12,16 @@ from luma.core import cmdline, error
 from luma.core.render import canvas
 from PIL import ImageFont
 
-# Interfaces de red que nos interesan
+# Net ifaces
 NET_INTERFACES = ('wlan0', 'eth0')
 
-# Dispositivos USB a monitorear
+# Monitoring
 OBSERVED_DEVICES = {
     "jieli":  {"vendor": 0x4c4a, "product": 0x4155},
     "c925e":  {"vendor": 0x046d, "product": 0x085b},
     "hagibis": {"vendor": 0x345f, "product": 0x2131},
 }
 
-# Ruta de la fuente
 FONT_PATH = Path(__file__).resolve().parent / 'fonts' / 'C&C Red Alert [INET].ttf'
 FONT = ImageFont.truetype(str(FONT_PATH), 12)
 
@@ -57,7 +56,7 @@ def get_temp():
     try:
         return round(CPUTemperature().temperature)
     except Exception:
-        return -1  # Puede fallar si no está disponible
+        return -1
 
 def get_ip_addresses(family=socket.AF_INET, interfaces=NET_INTERFACES):
     for iface, snics in psutil.net_if_addrs().items():
@@ -72,17 +71,18 @@ def find_usb_devices(devices):
         for name, ids in devices.items()
     }
 
-def get_current_speaker():
-    path = Path(__file__).resolve().parent / "current_speaker.txt"
-    if path.exists():
-        return path.read_text().strip()
-    return "Unknown"
+def get_status_oled():
+    try:
+        with open("/tmp/status-oled", "r") as f:
+            estado = f.read().strip()
+        return estado
+    except Exception as e:
+        return "UNKNOWN"
 
 def draw_status_screen(device):
     with canvas(device) as draw:
-        current_speaker = get_current_speaker()[:20].upper()
-        draw.text((0, 0), f'REC {current_speaker}', font=FONT, fill="white")
-
+        status_oled = get_status_oled()
+        draw.text((0, 0), f'{status_oled}', font=FONT, fill="white")
         if device.height >= 32:
             stats_line = f"C {cpu_usage()} M {mem_usage()} D {disk_usage()} T {get_temp()}"
             draw.text((0, 14), stats_line, font=FONT, fill="white")
@@ -100,8 +100,8 @@ def draw_status_screen(device):
             except usb.core.USBError as e:
                 draw.text((0, 38), f'USB ERROR: {str(e)}', font=FONT, fill="white")
 
-            # Placeholder de tiempo de grabación
-            draw.text((0, 50), 'TALK_2 33:12 ????', font=FONT, fill="white")
+            # LASTLINE
+            # draw.text((0, 50), 'TALK_2 33:12 ????', font=FONT, fill="white")
 
 def main():
     device = get_device()
