d59ad37ce1908e8a0a611ff7d9c7573d573fb03c
David Santos <44490090+dsecuma@users.noreply.github.com>
2025-05-18 11:28:46 +0200

    env file for ws password

--- oled_controller/buttons_controller.py
+++ oled_controller/buttons_controller.py
@@ -10,34 +10,42 @@ logging.basicConfig(
     format='%(asctime)s %(levelname)s: %(message)s'
 )
 
-def action_button1():
-    try:
-        logging.info("Recording...")
-    except Exception as e:
-        logging.error(f"Error starting recording: {e}")
+OBS_CLI = "YOUR_OBS-CLI_PATH"
+OBS_PASSW = os.environ.get("OBS_API_PASSWORD")
+OBS_PORT= "4455"
+OBS_CMD = [OBS_CLI, "-p", OBS_PASSW, "-P", OBS_PORT, "record"]
 
-def action_button2():
+def accion_boton1():
     try:
-        logging.info("Stopping...")
+        resultado = subprocess.run(OBS_CMD.append("start"), capture_output=True, text=True, check=True)
+        logging.info(f"Grabación iniciada {resultado.stdout}")
     except Exception as e:
-        logging.error(f"Error stopping recording: {e}")
+        logging.error(f"Error al iniciar grabación: {e}")
 
-def action_button3():
+def accion_boton2():
+    try:
+        resultado = subprocess.run(OBS_CMD.append("stop"), capture_output=True, text=True, check=True)
+        logging.info("Grabación detenida")
+    except Exception as e:
+        logging.error(f"Error al detener grabación: {e}")
+
+def accion_boton3():
     os.system("uptime > /tmp/button3")
     logging.info("Botón 3 pulsado")
 
-def action_button4():
-    os.system("touch /tmp/button4")
+def accion_boton4():
+    # require nopasswd for daemon user
+    os.system("sudo /sbin/reboot")
     logging.info("Botón 4 pulsado")
 
 buttons = {
-    Button(26): action_button1,
-    Button(16): action_button2,
-    Button(20): action_button3,
-    Button(19): action_button4,
+    Button(26): accion_boton1,
+    Button(16): accion_boton2,
+    Button(20): accion_boton3,
+    Button(19): accion_boton4,
 }
 
-for button, action in buttons.items():
-        button.when_pressed = action
+for boton, accion in buttons.items():
+    boton.when_pressed = accion
 
 pause()
